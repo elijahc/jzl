b91bf1391f87619d2922a4bbcc78a954d5c9fd00
Elijah Christensen <ejd.christensen@gmail.com>
2017-07-24 20:31:47 -0600

    updates to layouts

--- jzl/plotly/layouts.py
+++ jzl/plotly/layouts.py
@@ -1,5 +1,52 @@
 import plotly
 import plotly.graph_objs as go
 
-class TimeSeries(
-class PowerSpectrum(
+class DefaultLayout(object):
+    def __init__(self,
+            title='Default Title',
+            xlabel='X Axis',
+            ylabel='Y Axis'):
+        self.title = title
+        self.xlabel=xlabel
+        self.ylabel=ylabel
+        self._xaxis = None
+        self._yaxis = None
+
+    @property
+    def xaxis(self):
+        self._xaxis=dict(
+                    title = self.xlabel,
+                    titlefont=dict(
+                        family='Courier New, monospace',
+                        size=18,
+                        color='#7f7f7f',
+                        ),
+                    )
+        return self._xaxis
+
+    @property
+    def yaxis(self):
+        self._yaxis=dict(
+                    title = self.ylabel,
+                    titlefont=dict(
+                        family='Courier New, monospace',
+                        size=18,
+                        color='#7f7f7f',
+                        ),
+                    )
+        return self._yaxis
+
+    def build(self):
+        return go.Layout(
+                title=self.title,
+                xaxis=self.xaxis,
+                yaxis=self.yaxis
+                )
+
+class PowerSpectrumLayout(DefaultLayout):
+    def __init__(self,
+            xlabel='Time (s)',
+            ylabel='Frequency (Hz)',**kwargs):
+        self.xlabel=xlabel
+        self.ylabel=ylabel
+        super(PowerSpectrumLayout,self).__init__(xlabel=xlabel,ylabel=ylabel,**kwargs)
