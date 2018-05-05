e37bfd8987f4c0e3665ead229f62ac8450bd23b8
Elijah C <ejd.christensen@gmail.com>
2018-05-05 09:01:39 -0600

    added load_raw as function in kohn dataset

--- jzl/datasets/kohn.py
+++ jzl/datasets/kohn.py
@@ -4,10 +4,7 @@ import scipy.io as sio
 
 from ..utils.data_utils import get_file
 
-def load_dataset(dat_id):
-    if dat_id in ['01','02','03','04','05','06','07','08','09','10']:
-
-        mat_fp = os.path.expanduser('~','.datasets','kohn_v1','mat_files',dat_id+'.mat')
+def _load_mat(mat_fp):
         if not os.path.exists(mat_fp):
             # Download dataset
             url = 'https://www.dropbox.com/s/nk3u1a58ooyq6ik/kohn.tar.gz?dl=1'
@@ -20,3 +17,25 @@ def load_dataset(dat_id):
         mat_dat = sio.loadmat(mat_fp)
 
         return mat_dat
+
+def load_images(dat_id):
+    if dat_id in ['01','02','03','04','05','06','07','08','09','10']:
+
+        mat_fp = os.path.join(os.path.expanduser('~'),'.datasets','kohn_v1','mat_files',dat_id+'.mat')
+    else:
+        raise 'Bad dat_id must be like "02"'
+
+    dat = _load_mat(mat_fp)
+
+    return dat['images']
+
+def load_raw(dat_id):
+    if dat_id in ['01','02','03','04','05','06','07','08','09','10']:
+
+        mat_fp = os.path.join(os.path.expanduser('~'),'.datasets','kohn_v1','mat_files',dat_id+'.mat')
+    else:
+        raise 'Bad dat_id must be like "02"'
+
+    dat = _load_mat(mat_fp)
+
+    return dat
